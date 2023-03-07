d3c057abe44f5c55b27893b4c3c3549c826f780e
tarek421 <tarekmahmud740@gmail.com>
2023-03-07 10:38:28 +0600

    responsive check

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
             </div>
 
 
-            <label className="relative block mt-44 sm:mt-36">
+            <label className="relative block mt-40 sm:mt-36">
               <span className="sr-only">Search</span>
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-camera " viewBox="0 0 16 16"> <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" /> <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" /> </svg>
@@ -52,7 +52,7 @@ function App() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16" id="IconChangeColor" transform="rotate(45)"> <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" id="mainIconPathAttribute" fill="#ffffff"></path> </svg>
               </span>
 
-              <input class="placeholder:italic placeholder:text-slate-400 block bg-[#3D4354] w-[96%] sm:w-[376px] h-[46px] rounded-full mt-16 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm py-2 pl-12 pr-3" placeholder="Message" type="text" name="search" />
+              <input class="placeholder:italic placeholder:text-slate-400 block bg-[#3D4354] w-[96%] sm:w-[376px] h-[46px] rounded-full focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm py-2 pl-12 pr-3" placeholder="Message" type="text" name="search" />
             </label>
           </div>
         </div >
